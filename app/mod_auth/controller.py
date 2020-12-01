a70de81f93be89edd901b0adb76471eb2f45a282
Ryan Belan <ryan1231@comcast.net>
2020-11-30 22:01:01 -0800

    added routes

--- app/mod_auth/controller.py
+++ app/mod_auth/controller.py
@@ -1,35 +1,27 @@
 # Import flask dependencies
-from flask import Blueprint, request, render_template, \
-                  flash, g, session, redirect, url_for
-
-# Import password / encryption helper tools
-# from werkzeug import check_password_hash, generate_password_hash
-
-# Import the database object from the main app module
+from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
 from app.app import db
-
-# # Import module forms
-# from app.mod_auth.forms import LoginForm
-
-# # Import module models (i.e. User)
-# from app.mod_auth.models import User
+from app.mod_auth.forms import LoginForm
+from app.mod_auth.models import User
 
 # Define the blueprint: 'auth', set its url prefix: app.url/auth
-mod_auth = Blueprint('auth', __name__, url_prefix='/auth', template_folder='templates')
-
+mod_auth = Blueprint('auth', __name__)
 # Set the route and accepted methods
-@mod_auth.route('/signin/', methods=['GET', 'POST'])
+
+@mod_auth.route('/login')
 def signin():
+    return render_template("login.html")
+
+@mod_auth.route('/login', method=['POST'])
+#def signin_post():
 
     # If sign in form is submitted
-    form = LoginForm(request.form)
+    #form = LoginForm(request.form)
 
     # # Verify the sign in form
-    # if form.validate_on_submit():
-
-    #     user = User.query.filter_by(email=form.email.data).first()
-
-    #     if user and check_password_hash(user.password, form.password.data):
+    #if form.validate_on_submit():
+        # user = User.query.filter_by(email=form.email.data).first()
+      #  if user and check_password_hash(user.password, form.password.data):
 
     #         session['user_id'] = user.id
 
@@ -39,4 +31,16 @@ def signin():
 
     #     flash('Wrong email or password', 'error-message')
 
-    return render_template("auth/signin.html", form=form)
+    #return render_template("login.html")
+
+@mod_auth.route('/signup', method=['POST'])
+def signup():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    return redirect(url_for('auth.login'))
+
+
+@mod_auth.route('/logout')    
+def logout():
+    return redirect(url_for('index.html')) 
+
