28f276c3b781e9ba662d921747e2f1a1dadcc344
kgiomi <48394056+kgiomi@users.noreply.github.com>
2020-12-03 22:43:36 -0800

    my stuck spot ughh

--- app/mod_auth/controller.py
+++ app/mod_auth/controller.py
@@ -10,6 +10,7 @@ mod_auth = Blueprint('mod_auth', __name__)
 def login():
     form = LoginForm(request.form)
     if form.validate_on_submit():
+        return render_template('user/profile.html')
         username = request.form.get('username')
         password = request.form.get('password')
         user = User.query.filter_by(username=username).first()
@@ -17,12 +18,14 @@ def login():
             flash("Password or Username is Incorrect")
             return redirect(url_for('mod_auth.login'))
         login_user(user)
+        return render_template('user/profile.html')
     return render_template('auth/login.html', form=form)
 
 @mod_auth.route('/signup', methods=['POST','GET'])
 def signup():
     form = SignupForm(request.form)
     if form.validate_on_submit():
+        return render_template('user/profile.html')
         name = request.form.get('name')
         birth = request.form.get("birth")
         username = request.form.get('username')
@@ -34,6 +37,7 @@ def signup():
         user_info = User(name=name, birth=birth, username=username, password=password)
         db.session.add(user_info)
         db.session.commit()
+        return render_template('user/profile.html')
     return render_template("auth/signup.html", form=form)
 
 
