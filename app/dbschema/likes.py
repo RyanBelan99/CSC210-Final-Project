e8656da629877c1d0d52dceaf2d59b9672f31e45
kgiomi <48394056+kgiomi@users.noreply.github.com>
2020-12-04 20:31:51 -0800

    Update likes.py

--- app/dbschema/likes.py
+++ app/dbschema/likes.py
@@ -9,15 +9,3 @@ class LikePost(db.Model):
 
     def __repr__(self):
         return '<Like id %r>' % self.id
-
-# class LikeTotals(db.Model):
-#     __tablename__ = 'users'
-#     id = db.Column(db.Integer, primary_key = True, autoincrement=True)
-#     name = db.Column(db.String(20))
-#     birth = db.Column(db.String(10), nullable = False)
-#     username = db.Column(db.String(64), unique=True, index=True)
-#     password = db.Column(db.String(128))
-#     recipes = db.relationship('Recipe', backref='users', lazy=True)
-#
-#     def __repr__(self):
-#         return '<User %r>' % self.username
