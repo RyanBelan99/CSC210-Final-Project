0a779b8746f6385ff27783eb76017194d1629400
Ryan Belan <ryan1231@comcast.net>
2020-12-07 15:39:32 -0800

    more bugs

--- app/mod_post/controller.py
+++ app/mod_post/controller.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from app import db
 from app.dbschema.users import User
-from app.entities.models import PostForm, LikeForm
+from app.entities.models import PostForm, LikeForm, EditRecipeForm
 from app.dbschema.recipe import Recipe
 from app.dbschema.likes import LikePost
 from sqlalchemy import desc
@@ -43,21 +43,23 @@ def likePost(recipe_id):
         db.session.commit()
         return redirect(url_for("mod_post.posts"))
 
-@mod_post.route('/editPost/<int:recipe_id>', methods=['POST'])
+@mod_post.route('/editPost/<int:recipe_id>', methods=['POST','GET'])
 def editPost(recipe_id):
-    update = Recipe.query.get(recipe_id)
-    update.title = request.form['title']
-    print(request.form['item'])
-    update.ingredients = request.form['item']
-    print(update.ingredients)
-    print(update.instructions)
-    update.instructions = request.form['list']
-    print(update.instructions)
-    try:
-        db.session.commit()
-    except:
-        return "Error deleting from database"  
-    return redirect(url_for("mod_post.posts"))
+    edit_form = EditRecipeForm(request.edit_form)
+    if edit_form.validate_on_submit():
+        title = request.edit_form.get('title')
+        ingredients = request.edit_form.get('newIngredients')
+        instructions = request.edit_form.get('newInstructions')
+        recipe_user = Recipe.query.filter_by(id=recipe_id)
+        setattr(recipe_user, 'title', title)
+        setattr(recipe_user,'ingredient', ingredients)
+        setattr(recipe_user,'instructions', instructions)
+        try:
+            db.session.commit()
+        except:
+            return "Error editing from database"
+    else:
+        return render_template("post/posts.html", edit_form=edit_form)
 
 
 @mod_post.route('/deletePost/<recipe_id>', methods=['POST','GET'])
