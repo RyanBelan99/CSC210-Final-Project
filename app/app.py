1ca9e2741b3636a2d184eacfbbc19f7a3ccd4c54
Ryan Belan <ryan1231@comcast.net>
2020-12-08 13:37:57 -0800

    https

--- app/app.py
+++ app/app.py
@@ -1,3 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_migrate import Migrate
 from app import db, app
+
+if __name__ == "__main__":
+     app.run(ssl_context=('cert.pem', 'key.pem'))
