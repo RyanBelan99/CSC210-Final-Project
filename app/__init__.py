9e1ae1cd101865ee62d82a084e0d5a6d95dafd02
mocha1227 <kalen@frieberg.com>
2020-12-08 20:15:40 -0800

    ooh

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,10 @@
+import os
+import sys
+from app.app import create_app
+sys.path.append(os.path.dirname(__name__))
 
+
+application = create_app()
+
+if __name__ == "__main__":
+    app.run(use_reloader=False)
